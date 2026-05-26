5c2838644e7b79883f02139902337d2b49603a8d
Eddy Chen <e@eddychen.ca>
2026-05-25 23:42:33 -0700

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  pack: {
+    dts: true,
+    exports: true,
+    minify: {
+      compress: true,
+      mangle: true,
+    },
+  },
+  lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+  },
+  fmt: {
+    arrowParens: "avoid",
+    sortImports: true,
+    sortPackageJson: true,
+  },
+  test: {
+    include: ["tests/**/*.test.ts"],
+    coverage: {
+      reporter: ["text", "html"],
+    },
+  },
+});
