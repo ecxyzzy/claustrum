2c5521a4d9fcdf8334bc552ea8c7dbc5a70f90aa
Eddy Chen <e@eddychen.ca>
2026-05-28 20:45:50 -0700

    docs(safeint): make tsdoc better

--- src/numeric/SafeInt.ts
+++ src/numeric/SafeInt.ts
@@ -1,7 +1,11 @@
 import type { RichInt } from "@/numeric/RichInt";
 
 /**
- * Represents an input where a safe integer is expected. The function may throw
- * if it does not receive an integral `number` or a `RichInt` object.
+ * Represents an input where a safe integer is expected. A function or method
+ * consuming a SafeInt should throw if it does not receive an integral `number`,
+ * such that `-(2 ** 53 - 1) <= n <= (2 ** 53 - 1)`, or a `RichInt` object.
+ *
+ * For developers: This can be accomplished by passing a `SafeInt` directly into
+ * the `RichInt()` constructor.
  */
 export type SafeInt = number | RichInt;
