a5f5c4dfa99acde5f2eae8f9c31b3464b1793377
Eddy Chen <e@eddychen.ca>
2026-06-06 13:51:14 -0700

    feat(try): add methods

--- src/adt/Try.ts
+++ src/adt/Try.ts
@@ -1,6 +1,12 @@
 abstract class _Try<T> {
   abstract readonly type: "Success" | "Failure";
   abstract match<U>({ Success, Failure }: { Success: (x: T) => U; Failure: (x: unknown) => U }): U;
+  abstract failed<E = unknown>(): Try<E>;
+  abstract filterOrElse(f: (x: T) => unknown, errorFactory: (x: T) => unknown): Try<T>;
+  abstract get(): T;
+  abstract isSuccess(): this is Success<T>;
+  abstract isFailure(): this is Failure<T>;
+  abstract mapFailure<E = unknown>(f: (x: E) => unknown): Try<T>;
 }
 
 class _Success<T> extends _Try<T> {
@@ -13,9 +19,33 @@ class _Success<T> extends _Try<T> {
   match<U>({ Success }: { Success: (x: T) => U }) {
     return Success(this.v);
   }
+
+  failed<E = unknown>(): Try<E> {
+    return Failure(new TypeError("failed called on instance of Success"));
+  }
+
+  filterOrElse(f: (x: T) => unknown, errorFactory: (x: T) => unknown): Try<T> {
+    return f(this.v) ? this : Failure(errorFactory(this.v));
+  }
+
+  get(): T {
+    return this.v;
+  }
+
+  isSuccess(): this is Success<T> {
+    return true;
+  }
+
+  isFailure(): this is Failure<T> {
+    return false;
+  }
+
+  mapFailure<E = unknown>(_f: (x: E) => unknown): Try<T> {
+    return this;
+  }
 }
 
-class _Failure extends _Try<never> {
+class _Failure<T> extends _Try<T> {
   readonly type = "Failure";
 
   constructor(private readonly e: unknown) {
@@ -25,12 +55,40 @@ class _Failure extends _Try<never> {
   match<U>({ Failure }: { Failure: (x: unknown) => U }) {
     return Failure(this.e);
   }
+
+  failed<E = unknown>(): Try<E> {
+    return Success(this.e as E);
+  }
+
+  filterOrElse(_f: (x: T) => unknown, _errorFactory: (x: T) => unknown): Try<T> {
+    return this;
+  }
+
+  get(): T {
+    throw this.e;
+  }
+
+  isSuccess(): this is Success<T> {
+    return false;
+  }
+
+  isFailure(): this is Failure<T> {
+    return true;
+  }
+
+  mapFailure<E = unknown>(f: (x: E) => unknown): Try<T> {
+    try {
+      return Failure(f(this.e as E));
+    } catch (e) {
+      return Failure(e);
+    }
+  }
 }
 
 /**
  * Represents a computation that may throw.
  */
-export type Try<T> = Success<T> | Failure;
+export type Try<T> = Success<T> | Failure<T>;
 export const Try = <T>(f: () => T): Try<T> => {
   try {
     return Success(f());
@@ -40,7 +98,7 @@ export const Try = <T>(f: () => T): Try<T> => {
 };
 
 export type Success<T> = _Success<T>;
-export type Failure = _Failure;
+export type Failure<T> = _Failure<T>;
 
 export const Success = <T>(v: T): Try<T> => new _Success(v);
-export const Failure = (e: unknown): Try<never> => new _Failure(e);
+export const Failure = <T>(e: unknown): Try<T> => new _Failure(e);
