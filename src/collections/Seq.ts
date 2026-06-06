81cc30d772324e8e9c07f76cd8d6b747b8045fe5
Eddy Chen <e@eddychen.ca>
2026-06-06 13:53:09 -0700

    feat(seq): add methods

--- src/collections/Seq.ts
+++ src/collections/Seq.ts
@@ -1,12 +1,39 @@
+import { Maybe } from "@/adt/Maybe";
+import type { Awaitable } from "@/concurrent/Awaitable";
+import { Task } from "@/concurrent/Task";
+
 class _Seq<T> implements Iterable<T> {
   constructor(private readonly xs: readonly T[]) {}
 
-  static from<T>(this: void, it: Iterable<T>): Seq<T> {
-    return new _Seq([...it]);
+  catMaybes<U>(this: Seq<Maybe<U>>): Seq<U> {
+    return Seq.from(this.xs.filter(Maybe.isJust).map(Maybe.unwrap));
+  }
+
+  filter(f: (x: T) => unknown): Seq<T> {
+    return Seq.from(this.xs.filter(f));
+  }
+
+  find(f: (x: T) => unknown): Maybe<T> {
+    return Maybe(this.xs.find(f));
   }
 
   length(): number {
     return this.xs.length;
+  }
+
+  map<U>(f: (x: T) => U): Seq<U> {
+    return Seq.from(this.xs.map(f));
+  }
+
+  traverseTask<U>(f: (x: T) => Awaitable<U>): Task<Seq<U>> {
+    const xs = this.xs;
+    return Task(async () => {
+      const ys: U[] = [];
+      for (const x of xs) {
+        ys.push(await f(x));
+      }
+      return Seq.from(ys);
+    });
   }
 
   [Symbol.iterator]() {
