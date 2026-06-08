df9d9b4d5bcb4078c095c42afe06c753fee55992
Eddy Chen <e@eddychen.ca>
2026-06-07 17:42:50 -0700

    feat(dict): add entries, find, and statics

--- src/collections/Dict.ts
+++ src/collections/Dict.ts
@@ -1,8 +1,19 @@
+import type { Maybe } from "@/adt";
+import { LazySeq } from "@/collections/LazySeq";
+
 class _Dict<K, V> implements Iterable<[K, V]> {
   private readonly d: ReadonlyMap<K, V>;
 
   constructor(pairs: Iterable<[K, V]>) {
     this.d = new Map(pairs);
+  }
+
+  entries(): LazySeq<[K, V]> {
+    return LazySeq.from(this.d);
+  }
+
+  find(f: (x: [K, V]) => unknown): Maybe<[K, V]> {
+    return this.entries().find(f);
   }
 
   size(): number {
@@ -14,5 +25,24 @@ class _Dict<K, V> implements Iterable<[K, V]> {
   }
 }
 
+type Dict_constructor = {
+  <K, V>(...pairs: [K, V][]): Dict<K, V>;
+};
+
+type Dict_static = {
+  entries<K, V>(d: Dict<K, V>): LazySeq<[K, V]>;
+  from<V>(record: Record<string, V>): Dict<string, V>;
+  size<K, V>(d: Dict<K, V>): number;
+};
+
+type Dict_typeof = Dict_constructor & Dict_static;
+
 export type Dict<K, V> = _Dict<K, V>;
-export const Dict = <K, V>(...pairs: [K, V][]): Dict<K, V> => new _Dict(pairs);
+export const Dict: Dict_typeof = Object.assign<Dict_constructor, Dict_static>(
+  (...pairs) => new _Dict(pairs),
+  {
+    entries: d => d.entries(),
+    from: record => new _Dict(Object.entries(record)),
+    size: d => d.size(),
+  },
+);
