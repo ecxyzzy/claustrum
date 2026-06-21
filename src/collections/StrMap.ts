49798984b24abca3b9d8158eaefa7ef11b876c42
Eddy Chen <e@eddychen.ca>
2026-06-21 14:14:56 -0700

    feat: rename dict to StrMap

---
+++ src/collections/StrMap.ts
@@ -0,0 +1,57 @@
+import { Maybe } from "@/adt/Maybe";
+import type { Associative } from "@/collections/Associative";
+import { LazySeq } from "@/collections/LazySeq";
+
+class _StrMap<V> implements Associative<string, V> {
+  private readonly d: ReadonlyMap<string, V>;
+
+  constructor(pairs: Iterable<[string, V]>) {
+    this.d = new Map(pairs);
+  }
+
+  entries(): LazySeq<[string, V]> {
+    return LazySeq.from([...this]);
+  }
+
+  find(f: (x: [string, V]) => unknown): Maybe<[string, V]> {
+    return this.entries().find(f);
+  }
+
+  get(k: string): Maybe<V> {
+    return Maybe(this.d.get(k));
+  }
+
+  liftMaybes(): StrMap<Maybe<V>> {
+    return new _StrMap(this.entries().map(([k, v]) => [k, Maybe(v)]));
+  }
+
+  size(): number {
+    return this.d.size;
+  }
+
+  [Symbol.iterator]() {
+    return this.d[Symbol.iterator]();
+  }
+}
+
+type StrMap_constructor = {
+  <V>(...pairs: [string, V][]): StrMap<V>;
+};
+
+type StrMap_static = {
+  entries<V>(d: StrMap<V>): LazySeq<[string, V]>;
+  from<V>(record: Record<string, V>): StrMap<V>;
+  size<V>(d: StrMap<V>): number;
+};
+
+type StrMap_typeof = StrMap_constructor & StrMap_static;
+
+export type StrMap<V> = _StrMap<V>;
+export const StrMap: StrMap_typeof = Object.assign<StrMap_constructor, StrMap_static>(
+  (...pairs) => new _StrMap(pairs),
+  {
+    entries: d => d.entries(),
+    from: record => new _StrMap(Object.entries(record)),
+    size: d => d.size(),
+  },
+);
--- src/collections/index.ts
+++ src/collections/index.ts
@@ -1,4 +1,4 @@
-export { Dict } from "@/collections/Dict";
+export { StrMap } from "@/collections/StrMap";
 export { FSet } from "@/collections/FSet";
 export { LazySeq } from "@/collections/LazySeq";
 export { Seq } from "@/collections/Seq";
